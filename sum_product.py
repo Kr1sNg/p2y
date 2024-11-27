afcc7a8ba83920244528092e584b4dd160772692
Kr1sNg <piq.k0olz@gmail.com>
2024-11-27 14:18:29 +0100

    python

---
+++ sum_product.py
@@ -0,0 +1,14 @@
+#Please write a program which asks the user for two numbers. The program will then print out the sum and the product of the two numbers.
+
+#The program should function as follows:
+#Sample output
+
+#Number 1: 3
+#Number 2: 7
+#The sum of the numbers: 10
+#The product of the numbers: 21
+
+one = int(input("Number 1: "))
+two = int(input("Number 2: "))
+print(f"The sum of the numbers: {one + two}")
+print(f"The product of the numbers: {one * two}")
